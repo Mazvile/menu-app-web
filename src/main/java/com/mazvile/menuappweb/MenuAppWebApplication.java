a03892895789f2928f7d68a9a5b94066f3f5d1fb
Mazvile <ab.mazvile@gmail.com>
2019-03-10 15:16:15 +0200

    Initial commit

---
+++ src/main/java/com/mazvile/menuappweb/MenuAppWebApplication.java
@@ -0,0 +1,13 @@
+package com.mazvile.menuappweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MenuAppWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MenuAppWebApplication.class, args);
+	}
+
+}
