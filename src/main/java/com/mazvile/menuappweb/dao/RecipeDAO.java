e7266a33ac02a56fd8b24ec73bc9d45c430b29c4
Mazvile <ab.mazvile@gmail.com>
2019-04-09 22:25:10 +0300

    RecipeDAO removeRecipe and updateRecipe methods implementation

--- src/main/java/com/mazvile/menuappweb/dao/RecipeDAO.java
+++ src/main/java/com/mazvile/menuappweb/dao/RecipeDAO.java
@@ -46,10 +46,43 @@ public class RecipeDAO {
     }
 
     public int removeRecipe(Recipe recipe) {
+        if (recipe.getId() != null) {
+            try {
+                Connection connection = dataSource.getConnection();
+                Statement statement = connection.createStatement();
+                int result = statement.executeUpdate("DELETE FROM Product_List WHERE Recipe_Id = " + recipe.getId());
+                result += statement.executeUpdate("DELETE FROM Recipe WHERE Id = " + recipe.getId());
+                statement.close();
+                connection.close();
+                return result;
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
         return 0;
     }
 
     public int updateRecipe(Recipe recipe) {
+        if (recipe.getId() != null) {
+            try {
+                Connection connection = dataSource.getConnection();
+                Statement statement = connection.createStatement();
+                int result = statement.executeUpdate("DELETE FROM Product_List WHERE Recipe_Id = " + recipe.getId());
+                result += statement.executeUpdate("UPDATE Recipe " +
+                        "SET Name = \"" + recipe.getName() + "\", Description = \"" + recipe.getDescription()
+                        + "\", Type = \"" + recipe.getDishType() + "\" WHERE Id = " + recipe.getId());
+                for (Product product : recipe.getProducts()) {
+                    result += statement.executeUpdate("INSERT INTO Product_List" +
+                            "(Recipe_Id, Product_Id, Quantity) VALUES (" + recipe.getId() +
+                            ", " + product.getId() + ", " + product.getQuantity().getValue() + ")");
+                }
+                statement.close();
+                connection.close();
+                return result;
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
         return 0;
     }
 
