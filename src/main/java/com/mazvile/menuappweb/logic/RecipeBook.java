4931e1652eb154d1af861929281e8ce251eae228
Mazvile <ab.mazvile@gmail.com>
2019-04-11 23:11:05 +0300

    RecipeBook logic class remade to Spring bean

--- src/main/java/com/mazvile/menuappweb/logic/RecipeBook.java
+++ src/main/java/com/mazvile/menuappweb/logic/RecipeBook.java
@@ -1,24 +1,29 @@
 package com.mazvile.menuappweb.logic;
 
+import com.mazvile.menuappweb.dao.RecipeDAO;
 import com.mazvile.menuappweb.model.Product;
 import com.mazvile.menuappweb.model.Recipe;
 import com.mazvile.menuappweb.model.RecipeType;
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Component
 public class RecipeBook {
 
-    private List<Recipe> recipes = new ArrayList<>();
+    @Autowired
+    private RecipeDAO recipes;
 
     public List<Recipe> getRecipes() {
-        return recipes;
+        return recipes.getAllRecipes();
     }
 
     public List<Recipe> getRecipeByType(RecipeType type) {
         List<Recipe> filteredRecipes = new ArrayList<>();
-        for (Recipe recipe : recipes) {
+        for (Recipe recipe : recipes.getAllRecipes()) {
             if (recipe.getDishType().equals(type)) {
                 filteredRecipes.add(recipe);
             }
@@ -29,7 +34,7 @@ public class RecipeBook {
     public boolean addRecipe(String name, RecipeType type, List<Product> products) {
         if (!StringUtils.isBlank(name)) {
             Recipe recipe = new Recipe(name, type, products);
-            recipes.add(recipe);
+            recipes.addRecipe(recipe);
             return true;
         }
         return false;
@@ -37,7 +42,7 @@ public class RecipeBook {
 
     public boolean addRecipe(Recipe recipe) {
         if (!StringUtils.isBlank(recipe.getName())) {
-            recipes.add(recipe);
+            recipes.addRecipe(recipe);
             return true;
         }
         return false;
